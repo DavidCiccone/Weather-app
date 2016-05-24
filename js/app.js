5c4278e2ad957ef7a5c0d0cfb5dc0f62cbabf80c
DavidCiccone <davidkciccone@gmail.com>
2016-05-23 23:54:25 -0400

    feat: more accurate location

--- js/app.js
+++ js/app.js
@@ -1,21 +1,33 @@
 "use strict";
 
 //Finds users location be lat and lon
-		var lat = google.loader.ClientLocation.latitude;
+/*		var lat = google.loader.ClientLocation.latitude;
 		var lon = google.loader.ClientLocation.longitude;
 		var city = google.loader.ClientLocation.address.city;
+*/
+
+//More accurate user location
+function success(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+        
+  
 
 //applies the users city
-		document.getElementsByClassName("city")[0].innerHTML = city;
+		//document.getElementsByClassName("city")[0].innerHTML = city;
 	
 //Retrieves weather data
 		$.ajax({ url: "https://api.forecast.io/forecast/c199afcc2ac8c994a18563502f3e65bc/" + lat + "," + lon,
   				 dataType: 'jsonp',
   				 success: function(results){
-  					console.log(results);
-  					console.log(results.currently.temperature);
+  					
+  					//Temprature
   					document.getElementsByClassName("temp")[0].innerHTML = results.currently.temperature + "° F";
+  				 	
+  				 	//Short Summary
   				 	document.getElementsByClassName("summary")[0].innerHTML = results.currently.summary;
+  				 	
+  				 	//Long Summary
   				 	document.getElementsByClassName("summary-long")[0].innerHTML = results.daily.summary;
 
 					//animated icon
@@ -30,21 +42,19 @@
 
 		});	
 		               
-
-
-
-
-
-
-
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    console.log(latitude, longitude);
-	}
-
+}
 
   navigator.geolocation.getCurrentPosition(success);
+
+
+
+
+
+
+
+
+
+
 
 
 
